4badc27315e36dfb01f60953d0824808ea8365c8
Lucas Cabral <lucas.9cabral41@gmail.com>
2022-04-18 17:45:31 -0300

    Conclusão do projeto do Bate-Papo da Uol.

--- script.js
+++ script.js
@@ -26,11 +26,11 @@ function erroNomeUsado(error){
     }
 }
 
-function abrirSala(){
+function abrirSala(resposta){  
     mostrarSala();
     carregarMensagem();
-    mandarMensagem();
-    setInterval(carregarMensagem, 5000);
+    verificarUserOnline(); 
+    setInterval(carregarMensagem, 3000);
 }
 
 function mostrarSala(){
@@ -55,38 +55,95 @@ function listarMensagens(resposta){
     mensagensDoChat = resposta.data;
 
     const corporMensagem = document.querySelector(".corpo-mensagem");
-
+    corporMensagem.scrollIntoView(false);
     corporMensagem.innerHTML = "";
     let conteudoMensagem = "";
 
     for(let i=0; i<mensagensDoChat.length; i++){
-        conteudoMensagem += `<div class="msg-todos">
-                                        <div class="box-msg">
-                                            <span class="hora-msg">${mensagensDoChat[i].time}</span>
-                                            <span class="msg-direcionada-todos"><strong>${mensagensDoChat[i].from}</strong> para <strong>${mensagensDoChat[i].to}</strong>:  ${mensagensDoChat[i].text}</span>
-                                        </div>
-                                    </div>`
+        let tipoMensagem;
+        if(mensagensDoChat[i].type === "message"){
+            tipoMensagem = "msg-todos";
+        }
+        if(mensagensDoChat[i].text === "entra na sala..."){
+            tipostatus = "msg-sala";
+            conteudoMensagem += `<div class=${tipostatus}>
+                                                <div class="box-msg">
+                                                    <span class="hora-msg">${mensagensDoChat[i].time}</span>
+                                                    <span class="entrada-sala"><strong>${mensagensDoChat[i].from}</strong> entra na sala... </span>
+                                                </div>
+                                          </div>` 
+        }
+        if(mensagensDoChat[i].text === "sai da sala..."){
+            tipostatus = "msg-saida";
+            conteudoMensagem += `<div class=${tipostatus}>
+                                                <div class="box-saida">
+                                                    <span class="hora-msg">${mensagensDoChat[i].time}</span>
+                                                    <span class="saida-sala"><strong>${mensagensDoChat[i].from}</strong> Saiu da sala...</span>
+                                                </div>
+                                 </div>` 
+        }
+        if(mensagensDoChat[i].type === "private_message"){
+            tipoMensagem = "msg-direcionada-alguem";
+        }
+        if(mensagensDoChat[i].type !== "status"){
+            conteudoMensagem += `<div class=${tipoMensagem}>
+                                    <div class="box-msg">
+                                        <span class="hora-msg">${mensagensDoChat[i].time}</span>
+                                        <span class="msg-direcionada-todos"><strong>${mensagensDoChat[i].from}</strong> para <strong>${mensagensDoChat[i].to}</strong>:  ${mensagensDoChat[i].text}</span>
+                                    </div>
+                                 </div>` 
+        } 
+        
     }
 
-      corporMensagem.innerHTML = conteudoMensagem;
+    corporMensagem.innerHTML = conteudoMensagem;
 }
 
 function mandarMensagem(){
     mensagemDoParticipante = document.querySelector(".digite-mensagem").value;
+    console.log(mensagemDoParticipante);
+    document.querySelector(".digite-mensagem").value = '';
 
     mensagem = {
-        from:participante,
+        from:nomeUser,
         to:"Todos",
         text:mensagemDoParticipante,
         type:"message"
     }
     let resposta = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", mensagem);
-    resposta.then(renderizarMensagem)    
+    resposta.then(carregarMensagem)
 }
 
-function renderizarMensagem(){
-    carregarMensagem()
+
+function verificarUserOnline(){
+    console.log("Verificando....");
+    
+    setInterval(verificandoStatus,5000);
 }
+function verificandoStatus(){
+   const tempoOnline = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", participante); 
+    tempoOnline.then(usuarioOnline);
+    tempoOnline.catch(usuarioOffline);
+}
+
+
+function usuarioOnline(resposta){
+    console.log(resposta.status);
+    if(resposta.status === 200){
+        carregarMensagem()  ;
+    }
+}
+
+function usuarioOffline(error){
+    console.log(error.response.status); 
+    if(error.response.status === 400){
+        alert("Você foi desconectado da sala por inatividade");
+        window.location.reload();
+    }
+   
+    
+}
+
 
 
 
